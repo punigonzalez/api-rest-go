8da39f5ec441199759bc6ee7d9c64a553bccfd48
Santiago Gonzalez <punigonzalez07@gmail.com>
2024-11-01 14:33:14 -0300

    se agrega db

--- main.go
+++ main.go
@@ -2,71 +2,91 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
 )
 
-// tipo de dato *objeto?*
+// defino estructura de producto (crear clase)
 type product struct {
-	ID    string  `json:"id"`
+	ID    uint    ` gorm:"primaryKey;autoIncrement" json:"id"`
 	Name  string  `json:"name"`
 	Brand string  `json:"brand"`
 	Color string  `json:"color"`
 	Price float32 `json:"price"`
 }
 
-// instancio los objetos producto
-var products = []product{
+// inicializo un slice (instanciar objetos)
+/*var products = []product{
 	{ID: "1", Name: "Iphone 14", Brand: "Apple", Color: "Black", Price: 450},
 	{ID: "2", Name: "Iphone 15", Brand: "Apple", Color: "Midnight", Price: 650},
 	{ID: "3", Name: "Iphone 16", Brand: "Apple", Color: "Red", Price: 899.99},
+}*/
+
+var (
+	db       *gorm.DB
+	products []product
+	err      error
+)
+
+// config db
+func initDB() {
+	dsn := "root:@tcp(127.0.0.1:3306)/api_rest_goDB?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("No se pudo conectar a la base de datos:", err)
+	}
+
+	// migrar el modelo Product a la base de datos
+	db.AutoMigrate(&product{})
 }
 
 // funcion para obtener toodos los productos
 func getProducts(c *gin.Context) {
+	var products []product
+	db.Find(&products)
 	c.IndentedJSON(http.StatusOK, products)
 }
 
 // funcion para agregar producto
-func postProduct(c *gin.Context) {
+func addProduct(c *gin.Context) {
 	var newProduct product
 
 	if err := c.BindJSON(&newProduct); err != nil {
 		return
 	}
 
-	products = append(products, newProduct)
-
-	c.IndentedJSON(http.StatusCreated, products)
+	db.Create(&newProduct)
+	c.IndentedJSON(http.StatusCreated, newProduct)
 }
 
 // funcion para obtener producto por id
 func getProductById(c *gin.Context) {
 	id := c.Param("id")
 
-	for _, a := range products {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	var product product
+	result := db.First(&product, id)
+	if result.Error != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Producto no encontrado."})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Producto no encontrado."})
+	c.IndentedJSON(http.StatusOK, product)
 }
 
 // funcion para eliminar producto por id
 func deleteProductById(c *gin.Context) {
 	id := c.Param("id")
 
-	for index, a := range products {
-		if a.ID == id {
-			// Eliminar el producto de la lista
-			products = append(products[:index], products[index+1:]...)
-			c.IndentedJSON(http.StatusOK, gin.H{"message": "Producto eliminado."})
-			return
-		}
+	result := db.Delete(&product{}, id)
+	if result.RowsAffected == 0 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Producto no encontrado."})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Producto no encontrado."})
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Producto eliminado."})
 }
 
 func main() {
@@ -76,10 +96,13 @@ func main() {
 	fmt.Println("*                                          *")
 	fmt.Println("********************************************")
 
+	//INICIALIZO DB
+	initDB()
+
 	//rutas http
 	router := gin.Default()
 	router.GET("/products", getProducts)
-	router.POST("/addProduct", postProduct)
+	router.POST("/addProduct", addProduct)
 	router.GET("/products/:id", getProductById)
 	router.DELETE("/deleteProduct/:id", deleteProductById)
 
